dd4c482c753e72aba77c761acd16a2d86297d4fc
shiningdracon <shiningdracon@gmail.com>
2017-05-25 22:39:19 -0700

    Move EBNF from source code to README

--- Sources/BBCode.swift
+++ Sources/BBCode.swift
@@ -27,26 +27,6 @@ public enum BBCodeError : Error {
 }
 
 public class BBCode {
-    /* EBNF
-     <document> ::= <element> | <document> <element>
-     <element> ::= <tag> | <content>
-     <tag> ::= <opening_tag_1> | <opening_tag> <content> <closing_tag>
-     <opening_tag> ::= <opening_tag_1> | <opening_tag_2>
-     <opening_tag_1> ::= <tag_prefix> <tag_end>
-     <opening_tag_2> ::= <tag_prefix> '=' <attr> <tag_end>
-     <tag_prefix> ::= <tag_start> <tag_name>
-     <tag_start> ::= '['
-     <tag_end> ::= ']'
-     <attr> ::= <character> | <attr> <character>
-     <closing_tag> ::= <tag_start> '/' <tag_name> <tag_end>
-     <tag_name> ::= <character_non-key> | <tag_name> <character_non-key_2>
-     <content> ::= <character> | <content> <character>
-     <character> ::= unicode
-     <character_non-key> ::= unicode not including '[', '=', ']', '/'
-     <character_non-key_2> ::= <character_non-key> | '/'
-     */
-
-    
     typealias USIterator = String.UnicodeScalarView.Iterator
     typealias Render = (DOMNode) -> String
     typealias TagInfo = (String, BBType, TagDescription)

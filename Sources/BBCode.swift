9c7f7d2301ec1b03bcc577cbc7f60ee04cfeaa4b
shiningdracon <shiningdracon@gmail.com>
2017-08-14 16:09:02 -0700

    Add CJK to string escaping whitelist.

--- Sources/BBCode.swift
+++ Sources/BBCode.swift
@@ -828,7 +828,31 @@ extension String {
                 ret.append("&lt;")
             } else if c == UnicodeScalar(0x003E) {
                 ret.append("&gt;")
-            } else if c > UnicodeScalar(126) {
+            } else if c >= UnicodeScalar(0x3000 as UInt16)! && c <= UnicodeScalar(0x303F as UInt16)! {
+                // CJK 标点符号 (3000-303F)
+                ret.append(Character(c));
+            } else if c >= UnicodeScalar(0x3400 as UInt16)! && c <= UnicodeScalar(0x4DBF as UInt16)! {
+                // CJK Unified Ideographs Extension A (3400–4DBF) Rare
+                ret.append(Character(c));
+            } else if c >= UnicodeScalar(0x4E00 as UInt16)! && c <= UnicodeScalar(0x9FFF as UInt16)! {
+                // CJK Unified Ideographs (4E00-9FFF) Common
+                ret.append(Character(c));
+            } else if c >= UnicodeScalar(0xFF00 as UInt16)! && c <= UnicodeScalar(0xFFEF as UInt16)! {
+                // 全角ASCII、全角中英文标点、半宽片假名、半宽平假名、半宽韩文字母 (FF00-FFEF)
+                ret.append(Character(c));
+            } else if c >= UnicodeScalar(0x20000 as UInt32)! && c <= UnicodeScalar(0x2A6DF as UInt32)! {
+                // CJK Unified Ideographs Extension B (20000-2A6DF) Rare, historic
+                ret.append(Character(c));
+            } else if c >= UnicodeScalar(0x2A700 as UInt32)! && c <= UnicodeScalar(0x2B73F as UInt32)! {
+                // CJK Unified Ideographs Extension C (2A700–2B73F) Rare, historic
+                ret.append(Character(c));
+            } else if c >= UnicodeScalar(0x2B740 as UInt32)! && c <= UnicodeScalar(0x2B81F as UInt32)! {
+                // CJK Unified Ideographs Extension D (2B740–2B81F) Uncommon, some in current use
+                ret.append(Character(c));
+            } else if c >= UnicodeScalar(0x2B820 as UInt32)! && c <= UnicodeScalar(0x2CEAF as UInt32)! {
+                // CJK Unified Ideographs Extension E (2B820–2CEAF) Rare, historic
+                ret.append(Character(c));
+            } else if c > UnicodeScalar(0x7E) {
                 ret.append("&#\(UInt32(c));")
             } else {
                 ret.append(String(Character(c)))
